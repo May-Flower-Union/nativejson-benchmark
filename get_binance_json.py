3f2fe95497fcb8f4671ea975d11071676e02f679
andrejj7771 <andrejj6661@gmail.com>
2021-08-22 10:16:36 +0000

    added binance json grabber

---
+++ get_binance_json.py
@@ -0,0 +1,42 @@
+from binance.client import Client
+import json
+import sys
+
+if len(sys.argv) < 3:
+    print('need api key and secret key')
+    sys.exit(1)
+
+api_key = sys.argv[1]
+secret_key = sys.argv[2]
+
+pairs = ['ETHBTC','ADABTC','AVAXBTC','DOTBTC','LTCBTC','LUNABNB','SOLBNB','BAKEBNB','BTCBUSD','SOLUSDT', 'FILUPUSDT']
+limits = [250, 500, 1000, 2500, 5000]
+
+client = Client(api_key, secret_key)
+
+filenames = 'exchange_info.json\n'
+
+#get exchange info
+data = client.get_exchange_info()
+json.dump(data, open('data/exchange_info.json', 'w'), ensure_ascii=False)
+
+
+for pair in pairs:
+    for limit in limits:
+        data = client.get_order_book(symbol=pair, limit=250)
+        filename = 'orderBook_%s_limit_%s.json' % (pair, limit)
+        filenames += filename + '\n'
+        json.dump(data, open('data/' + filename, 'w'), ensure_ascii=False)
+
+    data = client.get_recent_trades(symbol=pair, limit=1000)
+    filename = 'recent_trades_%s_limit_1000.json' % pair
+    filenames += filename + '\n'
+    json.dump(data, open('data/' + filename, 'w'), ensure_ascii=False)
+
+    data = client.get_avg_price(symbol=pair)
+    filename = 'average_price_%s.json' % pair
+    filenames += filename + '\n'
+    json.dump(data, open('data/' + filename, 'w'), ensure_ascii=False)
+
+print(filenames, file=open("data/data.txt", 'w'))
+print('DONE!')
